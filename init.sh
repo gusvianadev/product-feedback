55a4b735e7d19a5bbb21ae16413a8691316fdd58
gusvianadev <gusvianadev@gmail.com>
2023-10-21 12:50:03 -0300

    no shuttle

--- init.sh
+++ init.sh
@@ -5,7 +5,11 @@ session="product-feedback"
 tmux new-session -d -s $session
 
 window=1
-tmux rename-window -t $session:$window "neovim"
+tmux rename-window -t $session:$window "neovim" c-m
 tmux send-keys -t $session:$window "v ." c-m
+
+window=2
+tmux new-window -t $session:$window -n "server"
+tmux send-keys -t $session:$window "cargo watch -x 'shuttle run --port 3000'" c-m
 
 tmux attach-session -t $session
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,43 @@
-use axum::{routing::get, Router};
+mod app;
+mod fallback;
 
-async fn hello_world() -> &'static str {
-    "Hello, world!"
+#[cfg(feature = "ssr")]
+#[tokio::main]
+async fn main() {
+    use crate::app::App;
+    use crate::fallback::file_and_error_handler;
+    use axum::{routing::post, Router};
+    use leptos::*;
+    use leptos_axum::{generate_route_list, LeptosRoutes};
+    use log::info;
+
+    simple_logger::init_with_level(log::Level::Info).expect("couldn't initialize logging");
+
+    // Setting get_configuration(None) means we'll be using cargo-leptos's env values
+    // For deployment these variables are:
+    // <https://github.com/leptos-rs/start-axum#executing-a-server-on-a-remote-machine-without-the-toolchain>
+    // Alternately a file can be specified such as Some("Cargo.toml")
+    // The file would need to be included with the executable when moved to deployment
+    let conf = get_configuration(None).await.unwrap();
+    let addr = conf.leptos_options.site_addr;
+    let leptos_options = conf.leptos_options;
+    // Generate the list of routes in your Leptos App
+    let routes = generate_route_list(App);
+
+    // build our application with a route
+    let app = Router::new()
+        .route("/api/*fn_name", post(leptos_axum::handle_server_fns))
+        .leptos_routes(&leptos_options, routes, || view! { <App/> })
+        .fallback(file_and_error_handler)
+        .with_state(leptos_options);
+    // run our app with hyper
+    // `axum::Server` is a re-export of `hyper::Server`
+    info!("listening on http://{}", &addr);
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
 }
 
-#[shuttle_runtime::main]
-async fn main() -> shuttle_axum::ShuttleAxum {
-    let router = Router::new().route("/", get(hello_world));
-
-    Ok(router.into())
-}
+#[cfg(not(feature = "ssr"))]
+pub fn main() {}
